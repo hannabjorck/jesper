d668bce44539a48f55b2db525faa4454bbe83f2c
Hanna Björck <hanbjo@cmm0352.ad.cmm.se>
2019-10-30 13:01:56 +0100

    vidareutvecklat script

--- analysis-all/2019-10-29-eQTL-INF.R
+++ analysis-all/2019-10-29-eQTL-INF.R
@@ -9,8 +9,8 @@ scriptsFunctions <- "functions-all"
 #läs in SNP data
 #header=TRUE tar bort V:na och gör så att första kolumnen blir heading
 
-#df <- read.table("../../data/191025_CHB_1E04_list.txt", header=TRUE) KORTARE LISTA (50 SNP ca)
-df <- read.table("../../data/191025_CHB_5E04_list.txt", header=TRUE)
+#df <- read.table("../../data/191025_CHB_1E04_list.txt", header=TRUE, stringsAsFactors=FALSE) KORTARE LISTA (50 SNP ca)
+df <- read.table("../../data/191025_CHB_5E04_list.txt", header=TRUE, stringsAsFactors=FALSE)
 
 #Ladda paketet GenomicRanges
 library("GenomicRanges")
@@ -20,26 +20,39 @@ library("GenomicRanges")
 #Detta kan man använda som input till readVcf som då kan hämta exakt de raderna från VCF filen
   
 gr <- GRanges(seqnames=df$CHR, IRanges(start=df$BP,end=df$BP), strand="*") 
-  #gr <- GRanges(seqnames=paste("chr",df$CHR,sep=""), IRanges(start=df$BP,end=df$BP), strand="*")
+#gr <- GRanges(seqnames=paste("chr",df$CHR,sep=""), IRanges(start=df$BP,end=df$BP), strand="*")
 
     
 #SNPs åker in som namn    
-  names(gr) <- df$SNP
+names(gr) <- df$SNP
 
-  gr2 <- gr[seqnames(gr)=="22"]
-  gr2
+gr2 <- gr[seqnames(gr)=="22"]
+gr2
   
 
 #Ladda paketet VariantAnnotation
-  library("VariantAnnotation")
+library("VariantAnnotation")
   
-    fl <- "../../data/vcfs/ASMADA_ASAP-MASAP_1KG_chr22.dose_maf-0.001_rsq-0.3-overlapping-variants.vcf.gz"
-    #param <- ScanVcfParam(which=gr2)
-    #vcf <- readVcf(fl, "hg19", param=gr2)
+fl <- "../../data/vcfs/ASMADA_ASAP-MASAP_1KG_chr22.dose_maf-0.001_rsq-0.3-overlapping-variants.vcf.gz"
+#param <- ScanVcfParam(which=gr2)
+#vcf <- readVcf(fl, "hg19", param=gr2)
     
-    vcf <- readVcf(fl, "hg19", param=ScanVcfParam(which=gr2))
-    vcf
+vcf <- readVcf(fl, "hg19", param=ScanVcfParam(which=gr2))
+vcf
     
+
+#Läs in genexpressionsdata
+heartexp <- read.table("../../data/2018-06-25-bed-expression-ASAP_H.bed", header=TRUE, comment.char = "&", stringsAsFactors=FALSE)
+
+#Gör ett GRanges objekt och ploppa in informationen.
+gr_exp <- GRanges(seqnames=heartexp$X.Chr, IRanges(start=heartexp$start,end=heartexp$end), strand="*") 
+
+#Ladda paketet SummarizedExperiment
+library("SummarizedExperiment")
+
+#Ta ut en rad (dvs. SNP) med dess genotyper. 
+
+
 
   
   
